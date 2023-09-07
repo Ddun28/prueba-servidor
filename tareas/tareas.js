07c1625dfa208e1ebd6d7d738e3c90961011ae52
Daniel Dun <danieldum28@gmail.com>
2023-09-07 15:23:04 -0400

    @Ddun28

---
+++ tareas/tareas.js
@@ -0,0 +1,8 @@
+//crear selectores
+const user = JSON.parse(localStorage.getItem('user'));
+//console.log(user)
+
+if(!user){
+    //caso de que el usuario no este en LS
+    window.location.href = '../home/index.html'
+}
