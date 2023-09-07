1d0daf88419eff03efc3ab7e3ed4f28e257461a9
Daniel Dun <danieldum28@gmail.com>
2023-09-07 15:33:47 -0400

    comienzo

--- tareas/tareas.js
+++ tareas/tareas.js
@@ -2,7 +2,24 @@
 const user = JSON.parse(localStorage.getItem('user'));
 //console.log(user)
 
+const formulario = document.querySelector('#form-todos');
+const lista = document.querySelector('#form-list');
+const inputF = document.querySelector('#form-input');
+const cerrarBtn = document.querySelector('#cerrar-btn');
+
 if(!user){
     //caso de que el usuario no este en LS
     window.location.href = '../home/index.html'
 }
+
+formulario.addEventListener('submit', async e=>{
+    e.preventDefault();
+
+    await fetch('http://localhost:3000/tareas',{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({text:inputF.value,nombre:user.nombre})
+    })
+})
